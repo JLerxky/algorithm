58a1bea5b1b926142a4e89054ced04e9a5a9c98f
JLer <472102489@qq.com>
2021-06-08 18:01:19 +0800

    coding-识别有效的IP地址和掩码并进行分类统计

--- hw/src/lib.rs
+++ hw/src/lib.rs
@@ -55,3 +55,125 @@ fn hj2() {
         }
     }
 }
+
+/// # 识别有效的IP地址和掩码并进行分类统计
+/// ## 描述
+/// 请解析IP地址和对应的掩码，进行分类识别。要求按照A/B/C/D/E类地址归类，不合法的地址和掩码单独归类。
+/// > 所有的IP地址划分为 A,B,C,D,E五类
+/// > A类地址1.0.0.0 ~ 126.255.255.255;
+/// > B类地址128.0.0.0 ~ 191.255.255.255;
+/// > C类地址192.0.0.0 ~ 223.255.255.255;
+/// > D类地址224.0.0.0 ~ 239.255.255.255；
+/// > E类地址240.0.0.0 ~ 255.255.255.255
+///
+/// > 私网IP范围是：
+/// > 10.0.0.0～10.255.255.255
+/// > 172.16.0.0～172.31.255.255
+/// > 192.168.0.0～192.168.255.255
+/// 子网掩码为二进制下前面是连续的1，然后全是0。（例如：255.255.255.32就是一个非法的掩码）
+/// 注意二进制下全是1或者全是0均为非法
+
+/// > 注意：
+/// > 1. 类似于【0.*.*.*】和【127.*.*.*】的IP地址不属于上述输入的任意一类，也不属于不合法ip地址，计数时可以忽略
+/// > 2. 私有IP地址和A,B,C,D,E类地址是不冲突的
+/// ## 输入描述：
+/// 多行字符串。每行一个IP地址和掩码，用~隔开。
+/// ## 输出描述：
+/// 统计A、B、C、D、E、错误IP地址或错误掩码、私有IP的个数，之间以空格隔开。
+/// ## 示例
+/// ```
+/// 输入： 10.70.44.68~255.254.255.0
+///       1.0.0.1~255.0.0.0
+///       192.168.0.2~255.255.255.0
+///       19..0.~255.255.255.0
+/// 输出： 1 0 1 0 0 2 1
+/// ```
+#[test]
+fn hj18() {
+    use std::io::{self, *};
+
+    let stdin = io::stdin();
+
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+    let mut d = 0;
+    let mut e = 0;
+    let mut err = 0;
+    let mut private = 0;
+
+    'line: for line in stdin.lock().lines() {
+        if let Ok(line) = line {
+            let text: Vec<&str> = line.split("~").collect();
+            // 识别子网掩码
+            let sub_mask = text[1];
+            let sub_mask_split: Vec<&str> = sub_mask.split(".").collect();
+            if sub_mask_split.len() != 4 {
+                err += 1;
+                break 'line;
+            }
+            let mut tmp = 255u8;
+            'mask: for mask in sub_mask_split {
+                if let Ok(mask) = mask.parse::<u8>() {
+                    println!("{:?}", mask);
+                    match mask {
+                        0 => {
+                            if tmp == 255 {
+                                tmp = 0;
+                            }
+                            continue 'mask;
+                        }
+                        255 => {
+                            if tmp == 255 {
+                                continue 'mask;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                err += 1;
+                break 'line;
+            }
+            // 识别IP
+            let ip = text[0];
+            let ip_split: Vec<&str> = ip.split(".").collect();
+            if ip_split.len() != 4 {
+                err += 1;
+                break 'line;
+            }
+            // ip第1位
+            if let Ok(ip1) = ip_split[0].parse::<u8>() {
+                println!("{:?}", ip1);
+                match ip1 {
+                    0 => {}
+                    1..=126 => {
+                        a += 1;
+                        if ip1 == 10 {
+                            private += 1;
+                        }
+                    }
+                    127 => {}
+                    128..=191 => {
+                        b += 1;
+                        if ip1 == 172 {
+                            let ip2 = ip_split[0].parse::<u8>();
+                        }
+                    }
+                    192..=223 => {
+                        c += 1;
+                    }
+                    224..=239 => {
+                        d += 1;
+                    }
+                    240..=255 => {
+                        e += 1;
+                    }
+                }
+            } else {
+                err += 1;
+                break 'line;
+            }
+        }
+    }
+    println!("{} {} {} {} {} {} {}", a, b, c, d, e, err, private);
+}
